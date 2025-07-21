8c6cc1584423b34321d9f930900d27bc4c15f917
Daniel Dorofeev <dorofeev.d.spb@gmail.com>
2025-07-21 21:19:33 +0300

    remove pvt-panel\lv_conf.h

--- pvt-panel/lv_conf.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define LV_COLOR_DEPTH 16
-#define LV_COLOR_16_SWAP 1          // для ST7789
-#define LV_USE_TFT_ESPI 1
-#define LV_TICK_CUSTOM 1
-#define LV_DPI_DEF 130
-#define LV_DRAW_COMPLEX 0
-#define LV_FONT_MONTSERRAT_14 1
