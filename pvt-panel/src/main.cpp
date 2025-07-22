77d3d6b711a2a8efec10dd14122c1398777889cc
Daniel Dorofeev <dorofeev.d.spb@gmail.com>
2025-07-22 18:43:27 +0300

    читай описание
    неправильно мапится координата проверь широту и долготу

--- pvt-panel/src/main.cpp
+++ pvt-panel/src/main.cpp
@@ -1,105 +1,117 @@
 
-
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 #include <lvgl.h>
 #include <SPI.h>
 
-#define TOUCH_CS 33
+
 #define CS_PIN 33
 SPIClass touchSPI(HSPI);
 TFT_eSPI tft = TFT_eSPI();
 XPT2046_Touchscreen touch(CS_PIN);
 
+
 // Буфер
 static lv_color_t buf[240 * 10];
 static lv_draw_buf_t draw_buf;
 
+
 // Отрисовка
-void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
-  uint32_t w = area->x2 - area->x1 + 1;
-  uint32_t h = area->y2 - area->y1 + 1;
-  tft.startWrite();
-  tft.setAddrWindow(area->x1, area->y1, w, h);
-  tft.pushColors((uint16_t *)px_map, w * h, false);
-  tft.endWrite();
-  lv_display_flush_ready(disp);
-}
+  void my_disp_flush
+       (lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+    {
+      uint32_t w = area->x2 - area->x1 + 1;
+      uint32_t h = area->y2 - area->y1 + 1;
+      tft.startWrite();
+      tft.setAddrWindow(area->x1, area->y1, w, h);
+      tft.pushColors((uint16_t *)px_map, w * h, false);
+      tft.endWrite();
+      lv_display_flush_ready(disp);
+    }
+
 
 // Сенсор
-void my_touch_read(lv_indev_t *indev, lv_indev_data_t *data) {
-  if (touch.touched()) {
-    TS_Point p = touch.getPoint();
-    data->point.x = map(p.x, 270, 3788, 0, 240);    
-    data->point.y = map(p.y, 480, 3783, 0, 320);
-    data->state = LV_INDEV_STATE_PRESSED;
-    Serial.printf("Touch: x=%d y=%d\n", data->point.x, data->point.y);
-  } else {
-    data->state = LV_INDEV_STATE_RELEASED;
+void my_touch_read(lv_indev_t *indev, lv_indev_data_t *data)
+  {
+    if (touch.touched())
+      {
+        TS_Point p = touch.getPoint();
+        data->point.x = map(p.x, 270, 3788, 0, 240);    
+        data->point.y = map(p.y, 480, 3783, 0, 320);
+        data->state = LV_INDEV_STATE_PRESSED;
+        Serial.printf("Touch: x=%d y=%d\n", data->point.x, data->point.y);
+      }
+    else
+      {
+        data->state = LV_INDEV_STATE_RELEASED;
+      }
   }
-}
+
 
 // Состояние кнопки
 static bool isRed = true;
 
-static void btn_event_cb(lv_event_t *e) {
-  Serial.println("Button clicked!");
-  isRed = !isRed;
-  uint32_t color = isRed ? 0xFF0000 : 0x0000FF;
-  lv_obj_t *btn = (lv_obj_t *)lv_event_get_target(e);
-  lv_obj_set_style_bg_color(btn, lv_color_hex(color), LV_PART_MAIN);
-  Serial.println(isRed ? "#FF0000" : "#0000FF");
-}
+static void btn_event_cb(lv_event_t *e)
+  {
+    Serial.println("Button clicked!");
+    isRed = !isRed;
+    uint32_t color = isRed ? 0xFF0000 : 0x0000FF;
+    lv_obj_t *btn = (lv_obj_t *)lv_event_get_target(e);
+    lv_obj_set_style_bg_color(btn, lv_color_hex(color), LV_PART_MAIN);
+    Serial.println(isRed ? "#FF0000" : "#0000FF");
+  }
 
 
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+  {
+    Serial.begin(115200);
 
-  tft.begin();
-  tft.setRotation(1);
-  pinMode(TFT_BL, OUTPUT);
-  digitalWrite(TFT_BL, HIGH);
-  touchSPI.begin(25, 39, 32, CS_PIN); // CLK, MISO, MOSI, CS
+    tft.begin();
+    tft.setRotation(1);
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
+    touchSPI.begin(25, 39, 32, CS_PIN); // CLK, MISO, MOSI, CS
     touch.begin(touchSPI);               // <-- важно!
 
-  lv_init();
+    lv_init();
 
-  // Инициализация draw_buf (6 аргументов!)
-  lv_draw_buf_init(&draw_buf, 240, 10, LV_COLOR_FORMAT_RGB565,
-                   240 * sizeof(lv_color_t), buf, sizeof(buf));
+    // Инициализация draw_buf
+    lv_draw_buf_init(&draw_buf, 240, 10, LV_COLOR_FORMAT_RGB565,
+                    240 * sizeof(lv_color_t), buf, sizeof(buf));
 
-  lv_display_t *disp = lv_display_create(240, 320);
-  lv_display_set_flush_cb(disp, my_disp_flush);
-  lv_display_set_draw_buffers(disp, &draw_buf, NULL);
+    lv_display_t *disp = lv_display_create(240, 320);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+    lv_display_set_draw_buffers(disp, &draw_buf, NULL);
 
-  lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_90); // поворот дисплея 
+    lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_90); // поворот дисплея 
 
-  lv_indev_t *indev = lv_indev_create();
-  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
-  lv_indev_set_read_cb(indev, my_touch_read);
+    lv_indev_t *indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+    lv_indev_set_read_cb(indev, my_touch_read);
 
-  // Кнопка
-  lv_obj_t *btn = lv_button_create(lv_screen_active());
-  lv_obj_set_size(btn, 120, 60);
-  lv_obj_center(btn);
-  lv_obj_set_style_bg_color(btn, lv_color_hex(0xFF0000), LV_PART_MAIN);
-  lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_CLICKED, NULL);
+    // Кнопка
+    lv_obj_t *btn = lv_button_create(lv_screen_active());
+    lv_obj_set_size(btn, 120, 60);
+    lv_obj_center(btn);
+    lv_obj_set_style_bg_color(btn, lv_color_hex(0xFF0000), LV_PART_MAIN);
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_CLICKED, NULL);
 
-  lv_obj_t *label = lv_label_create(btn);
-  lv_label_set_text(label, "TOUCH");
-  lv_obj_center(label);
-}
+    lv_obj_t *label = lv_label_create(btn);
+    lv_label_set_text(label, "TOUCH");
+    lv_obj_center(label);
+  }
 
-void loop() {
-  lv_timer_handler();
-  delay(5);
+void loop()
+  {
+    lv_timer_handler();
+    delay(5);
 
-if (touch.touched()) {
-  TS_Point p = touch.getPoint();
-  Serial.printf("Raw: x=%d y=%d\n", p.x, p.y);
-}
+    if (touch.touched())
+    {
+      TS_Point p = touch.getPoint();
+      Serial.printf("Raw: x=%d y=%d\n", p.x, p.y);
+    }
 
-
-}
+  }
