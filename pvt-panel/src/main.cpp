5730e4cc04edf58773f2ed1629fcfe4beec180e4
Daniel Dorofeev <dorofeev.d.spb@gmail.com>
2025-08-12 22:18:38 +0000

    кнопка есть цвета сьехали

--- pvt-panel/src/main.cpp
+++ pvt-panel/src/main.cpp
@@ -1,71 +1,106 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
 #include <lvgl.h>
-#include "ui/ui.h"      // сгенерирован EEZ-Studio
+#include "ui/ui.h"   // сгенерирован EEZ-Studio
 
-/* ---------- TFT-eSPI ---------- */
-TFT_eSPI tft = TFT_eSPI();
+/* ---------- настройки SPI для тач ---------- */
+#define TOUCH_CS 33
+SPIClass touchSPI(HSPI);
+TFT_eSPI tft = TFT_eSPI();                 // дисплей
+XPT2046_Touchscreen touch(TOUCH_CS);       // тач
 
-/* ---------- LVGL дисплей-буферы ---------- */
+/* ---------- буферы LVGL ---------- */
 #define BUF_LINES 24
 static lv_color_t buf1[240 * BUF_LINES];
 static lv_color_t buf2[240 * BUF_LINES];
 
-/* ---------- Переменная для кода EEZ ---------- */
-bool isRed = true;
+/* ---------- прототипы ---------- */
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map);
+void my_touch_read(lv_indev_t *indev, lv_indev_data_t *data);
+
+/* ---------- setup ---------- */
+void setup()
+{
+  Serial.begin(115200);
+
+  /* Инициализация дисплея */
+  tft.begin();
+  tft.setRotation(3); // usb left
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+
+  /* Инициализация тача */
+  touchSPI.begin(25, 39, 32, TOUCH_CS); // CLK, MISO, MOSI, CS
+  touch.begin(touchSPI);
+
+  /* Инициализация LVGL */
+  lv_init();
+  lv_display_t *disp = lv_display_create(240, 320);
+  lv_display_set_flush_cb(disp, my_disp_flush);
+  lv_display_set_buffers(disp, buf1, buf2, sizeof(buf1), LV_DISPLAY_RENDER_MODE_PARTIAL);
+  lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270); // usb left
+
+  lv_indev_t *indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  lv_indev_set_read_cb(indev, my_touch_read);
+
+  /* Запуск UI из EEZ-Studio */
+  ui_init();
+  lv_scr_load(objects.main); // добавляем экраны из EEZ-Studio 
+
+  //ui_init();
+  create_screens();     // если вдруг не вызвано
+  loadScreen(SCREEN_ID_MAIN);
+
+
+
+  //lv_obj_set_size(bg, 320, 240);                    // test
+  //lv_obj_set_style_bg_color(bg, lv_color_hex(0xFF0000), LV_PART_MAIN); // test
+  //lv_obj_center(bg);                   // test                            
+
+// tft.fillScreen(TFT_RED);//test
+// delay(1000);//test
+  
+// --- проверим, жив ли LV-драйвер ---
+// lv_obj_t *lbl = lv_label_create(lv_screen_active());
+// lv_label_set_text(lbl, "Hello LVGL");
+// lv_obj_center(lbl);
+
+
+}
+
+/* ---------- loop ---------- */
+void loop()
+{
+  lv_timer_handler();
+  delay(5);
+}
 
 /* ---------- LVGL flush-callback ---------- */
 void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
 {
-    uint32_t w = area->x2 - area->x1 + 1;
-    uint32_t h = area->y2 - area->y1 + 1;
-    tft.startWrite();
-    tft.setAddrWindow(area->x1, area->y1, w, h);
-    tft.pushColors((uint16_t *)px_map, w * h, false);
-    tft.endWrite();
-    lv_display_flush_ready(disp);
+  uint32_t w = area->x2 - area->x1 + 1;
+  uint32_t h = area->y2 - area->y1 + 1;
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, w, h);
+  tft.pushColors((uint16_t *)px_map, w * h, false);
+  tft.endWrite();
+  lv_display_flush_ready(disp);
 }
 
 /* ---------- LVGL touch-callback ---------- */
 void my_touch_read(lv_indev_t *indev, lv_indev_data_t *data)
 {
-    uint16_t x, y;
-    data->state = tft.getTouch(&x, &y) ? LV_INDEV_STATE_PRESSED
-                                        : LV_INDEV_STATE_RELEASED;
-    data->point.x = x;
-    data->point.y = y;
-}
-
-/* ---------- Инициализация ---------- */
-void setup()
-{
-    Serial.begin(115200);
-
-    tft.begin();
-    tft.setRotation(1);          // landscape
-    pinMode(TFT_BL, OUTPUT);
-    analogWrite(TFT_BL, 180);    // подсветка
-
-   lv_init();
-   lv_display_t *disp = lv_display_create(240, 320);
-   lv_display_set_flush_cb(disp, my_disp_flush);
-   lv_display_set_buffers(disp, buf1, buf2, sizeof(buf1), LV_DISPLAY_RENDER_MODE_PARTIAL);
-   lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_90);
-
-
-
-    
-
-    lv_indev_t *indev = lv_indev_create();
-    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
-    lv_indev_set_read_cb(indev, my_touch_read);
-
-    ui_init();   // запуск UI из EEZ-Studio
-}
-
-/* ---------- Главный цикл ---------- */
-void loop()
-{
-    lv_timer_handler();
-    delay(5);
+  if (touch.touched())
+  {
+    TS_Point p = touch.getPoint();
+    data->point.x = map(p.y, 600, 3700, 0, 240);
+    data->point.y = map(p.x, 420, 3788, 0, 320);
+    data->state = LV_INDEV_STATE_PRESSED;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
 }
--- pvt-panel/src/ui/screens.c
+++ pvt-panel/src/ui/screens.c
@@ -19,13 +19,31 @@ void create_screen_main() {
     objects.main = obj;
     lv_obj_set_pos(obj, 0, 0);
     lv_obj_set_size(obj, 320, 240);
+    lv_obj_set_style_bg_color(obj, lv_color_hex(0xff994d4d), LV_PART_MAIN | LV_STATE_DEFAULT);
     {
         lv_obj_t *parent_obj = obj;
         {
             lv_obj_t *obj = lv_label_create(parent_obj);
+            objects.obj0 = obj;
             lv_obj_set_pos(obj, 116, 120);
             lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_obj_set_style_text_color(obj, lv_color_hex(0xffffffff), LV_PART_MAIN | LV_STATE_DEFAULT);
             lv_label_set_text(obj, "Hello, world!");
+        }
+        {
+            lv_obj_t *obj = lv_button_create(parent_obj);
+            lv_obj_set_pos(obj, 75, 79);
+            lv_obj_set_size(obj, 172, 99);
+            {
+                lv_obj_t *parent_obj = obj;
+                {
+                    lv_obj_t *obj = lv_label_create(parent_obj);
+                    lv_obj_set_pos(obj, 0, 0);
+                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+                    lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
+                    lv_label_set_text(obj, "Button");
+                }
+            }
         }
     }
     
